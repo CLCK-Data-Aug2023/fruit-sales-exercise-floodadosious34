c2d1b309b69fb5863bfa1013b78c20acad4b3255
James Flood Jr <floodadosious111@gmail.com>
2023-12-12 18:50:58 -0500

    first commit'

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,9 @@
 # add your code here
+import pandas as pd
+
+print("setup pandas")
+
+fruit_sales = pd.DataFrame({'Apples': [35, 41], 'Bananas': [21, 34]}, index=['2017 Sales', '2018 Sales'])
+print(fruit_sales)
+
+fruit_sales.to_csv('fruit.csv')
